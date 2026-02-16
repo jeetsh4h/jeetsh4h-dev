5893669d82f2f2d774aea619dc400c9017b41f5e
jeetsh4h <jeetsh4h@gmail.com>
2026-02-16 23:22:39 +0530

    accessibility change

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
+  maximumScale: 2.5,
   // This ensures the layout resizes when the keyboard opens
   interactiveWidget: "resizes-content",
 };

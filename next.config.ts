96a57d0153feadebf986c63809df13528108218c
jeetsh4h <jeetsh4h@gmail.com>
2026-02-21 01:54:14 +0530

    fix commit SHA

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,13 @@ import { type NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   reactCompiler: true,
+  /* the commit used for terminal prompt */
+  // VERCEL_GIT_COMMIT_SHA for vercel
+  // GITHUB_SHA for github actions
+  // HEAD for local
   env: {
-    NEXT_PUBLIC_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA!,
+    NEXT_PUBLIC_COMMIT_SHA:
+      process.env.VERCEL_GIT_COMMIT_SHA || process.env.GITHUB_SHA || "HEAD",
   },
   images: {
     remotePatterns: [

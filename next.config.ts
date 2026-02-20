4082ef6dc6ac124f8ec9e89562442f1258ce3752
jeetsh4h <jeetsh4h@gmail.com>
2026-02-20 18:21:26 +0530

    add spotify to prompt

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import { type NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   reactCompiler: true,
+  env: {
+    NEXT_PUBLIC_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA || "local",
+  },
 };
 
 export default nextConfig;

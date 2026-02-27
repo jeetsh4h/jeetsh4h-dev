52d0282ed67cb6a4faaa1089cfc56d18939dc968
jeetsh4h <jeetsh4h@gmail.com>
2026-02-27 11:19:07 +0530

    project card clickable

--- components/projects.tsx
+++ components/projects.tsx
@@ -12,23 +12,22 @@ export default function Projects() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 -mt-2">
         {PROJECTS.map((project) => (
-          <Card
-            className="p-5 rounded-md border-term-border/50 group hover:ring-accent transition-colors cursor-pointer"
+          <Link
+            href={project.link}
+            target="_blank"
             key={project.link}
           >
-            <CardHeader className="flex justify-between items-start p-0">
-              <Link
-                className="text-primary underline decoration-primary/30 group-hover:decoration-primary transition-all"
-                href={project.link}
-                target="_blank"
-              >
-                <h3 className="font-bold text-lg ">{project.title}</h3>
-              </Link>
-            </CardHeader>
-            <p className="text-sm text-foreground leading-relaxed">
-              {project.description}
-            </p>
-          </Card>
+            <Card className="p-5 rounded-md border-term-border/50 group hover:ring-accent transition-colors cursor-pointer">
+              <CardHeader className="flex justify-between items-start p-0">
+                <h3 className="text-primary underline decoration-primary/30 group-hover:decoration-primary transition-allfont-bold text-lg">
+                  {project.title}
+                </h3>
+              </CardHeader>
+              <p className="text-sm text-foreground leading-relaxed">
+                {project.description}
+              </p>
+            </Card>
+          </Link>
         ))}
       </div>
     </>

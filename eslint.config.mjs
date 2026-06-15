c0c22fb6fc425226a572ed3823ec429a5618dc93
Jeet Shah <jeetsh4h@gmail.com>
2026-06-15 13:20:11 +0530

    chore(format): add prettier and mdx linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import * as mdx from "eslint-plugin-mdx";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    ...mdx.flat,
+    files: ["**/*.{md,mdx}"],
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
